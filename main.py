823ba9ad6a0f0b2aad423cd3fc06dce7e0d8c38d
Elias Hawa <elias.hawa2468@gmail.com>
2023-09-16 14:58:36 -0400

    Upload deploy stuff

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from taipy import Gui
+import os
+import taipy as tp
 
 # A dark mode is available in Taipy
 # However, we will use the light mode for the Getting Started
-Gui(page="# Getting started with *Taipy*").run(dark_mode=False)
+gui = tp.Gui(page="# Getting started with *Taipy*").run(dark_mode=False)
+
+tp.run(
+    title="Taipy Demo",
+    host='0.0.0.0',
+    port=os.environ.get('PORT', '5000'),
+)

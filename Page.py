d72e8b2bb8af585232f52c8100aa3312097dc8ba
Gabriel Luyanda <Gabriel.Luyanda14@gmail.com>
2023-09-17 03:49:55 -0400

    Website mvp draft

--- Page.py
+++ Page.py
@@ -4,7 +4,7 @@ import json
 import base64
 import numpy
 
-data = requests.get('https://testnet.mirrornode.hedera.com/api/v1/topics/0.0.2006098/messages').text
+data = requests.get('https://testnet.mirrornode.hedera.com/api/v1/topics/0.0.2009473/messages').text
 
 data_Dict = json.loads(data)["messages"]
 
@@ -13,19 +13,51 @@ x = []
 for row in data_Dict:
     s = base64.b64decode(row['message']).decode('utf-8')
     temp = (s).split(',')
-    if(len(temp)==4):
+    if(len(temp)==6):
         x.append(temp)
 
-columns = "Name;val;val;val"
+Transmitters = {}
 
-column_orders = [("X;Y;Z;W", "Squared"), ("Y;X", "Square root")]
+for item in x:
+    if item[0] not in Transmitters:
+        Transmitters[item[0]] = []
+    
+    Transmitters[item[0]].append(item)
+
+
+Values = []
+
+for key in Transmitters:
+    t = Transmitters[key]
+    t = numpy.transpose(t)
+
+    v = {
+        #"Name":t[0],
+        "CO2":[t[1][0]],
+        "TVOC":[t[2][0]],
+        "Temperature":[t[3][0]],
+        "Lattitude":[t[4][0]],
+        "Longitude":[t[5][0]]
+    }
+    print(v)
+    Values.append(v)
+
+list_to_display = [100/i for i in range(1, 100)]
 
 
 page = """
 # *PyroGuard*
 
-<|{x}|table|columns={x[0]}|show_all|>
+<|Transmitter A|expandable|
+<|{list_to_display}|chart|>
+<|{Values[0]}|table|>
+|>
 
+<|Transmitter B|expandable|
+<|{list_to_display}|chart|>
+<|{Values[1]}|table|>
+|>
 """
+
 
 Gui(page).run(use_reloader=True)
--- main.py
+++ main.py
@@ -2,9 +2,88 @@ import os
 import webbrowser
 from taipy import Gui
 from taipy.gui import Markdown
+import requests
+import json
+import base64
+import numpy
+import time
+
 
 def navigate_to_map(state):
   webbrowser.open_new_tab("http://127.0.0.1:5000/map.html")
+
+
+data = requests.get('https://testnet.mirrornode.hedera.com/api/v1/topics/0.0.2009473/messages').text
+
+data_Dict = json.loads(data)["messages"]
+
+x=[]
+
+
+for row in data_Dict:
+    d =time.ctime(float(row["consensus_timestamp"]))
+    s = base64.b64decode(row['message']).decode('utf-8')
+    temp = (s).split(',')
+    if(len(temp)==6):
+        temp.append(d)
+        x.append(temp)
+
+
+Transmitters = {}
+
+for item in x:
+    if item[0] not in Transmitters:
+        Transmitters[item[0]] = []
+    
+    Transmitters[item[0]].append(item)
+
+
+Values = []
+
+ChartValues = []
+
+for key in Transmitters:
+    t = Transmitters[key]
+    t = numpy.transpose(t)
+
+    v = {
+        #"Name":t[0],
+        "CO2":[t[1][0]],
+        "TVOC":[t[2][0]],
+        "Temperature":[t[3][0]],
+        "Lattitude":[t[4][0]],
+        "Longitude":[t[5][0]]
+    }
+
+    Values.append(v)
+
+    v = {
+        #"Name":t[0],
+        "Time": t[6],
+        "Temperature":t[3],
+        #"Lattitude":t[4],
+        #"Longitude":t[5]
+    }
+
+    ChartValues.append(v)
+
+
+page2_md = """
+
+<|Transmitter A|expandable|expanded=true|
+<|{ChartValues[0]}|chart|mode=lines|x=Time|y=Temperature|color=red|>
+# 
+Current Values
+<|{Values[0]}|table|show_all|>
+|>
+# 
+<|Transmitter B|expandable|expanded=false|
+<|{ChartValues[1]}|chart|mode=lines|x=Time|y=Temperature|color=red|>
+# 
+Current Values
+<|{Values[1]}|table|show_all|>
+|>
+"""
 
 # Add a navbar to switch from one page to the other
 root_md = """
@@ -13,18 +92,16 @@ root_md = """
 <|layout|columns=1fr auto 1fr|class_name=container align_columns_center|
 <|part|>
 <|part|class_name=align_item_stretch|
-# Pyro-Guard
+# *Pyro-Guard*
 |>
 <|part|>
 |>
 """
 about_md = Markdown("about.md")
-page2_md = "## This is page 2"
 
 pages = {
     "/": root_md,
-    "about": about_md,
-    "Transmitter-A": page2_md,
-    "Transmitter-B": page2_md,
+    "Data": page2_md,
+    "About": about_md,
 }
 Gui(pages=pages).run(use_reloader=True)
